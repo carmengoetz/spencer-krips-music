6c3ec18629e64a9c07c68156be756cd0363c0ade
Carmen Goetz <carmen.b.goetz@gmail.com>
2022-02-17 22:07:12 -0600

    clean up product cards

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card
     class="product d-flex flex-column px-4 py-6"
-    height="350"
+    height="372"
     elevation="0"
     color="transparent"
     tile
@@ -10,16 +10,18 @@
       params: { id: id },
     }"
   >
-    <v-img width="250" :src="image" class="align-self-center"
+    <v-img width="262" height="262" :src="image" class="align-self-center"
       ><template v-slot:placeholder>
         <v-row class="fill-height ma-0" align="center" justify="center">
           <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
         </v-row> </template
     ></v-img>
     <v-spacer></v-spacer>
-    <div class="product__name mt-4 ml-4">{{ name }}</div>
-    <div class="product__format ml-4">{{ format }}</div>
-    <div class="product__price mb-4 ml-4">CA${{ price }}.00</div>
+    <div class="product__desc mb-4 mx-2">
+      <div class="product__name mt-4 ml-4">{{ name }}</div>
+      <div class="product__format ml-4">{{ format }}</div>
+      <div class="product__price pb-4 ml-4">CA${{ price }}.00</div>
+    </div>
   </v-card>
 </template>
 
@@ -44,6 +46,13 @@ export default Vue.extend({
     background-color: #015869 !important;
   }
 
+  &__desc {
+    background-color: #015869;
+
+    @media (max-width: 1240px) {
+      background-color: transparent;
+    }
+  }
   &__name,
   &__price,
   &__format {
@@ -51,7 +60,7 @@ export default Vue.extend({
     font: normal normal normal 16px raleway, sans-serif;
     text-align: start !important;
 
-    @media (max-width: 600px) {
+    @media (max-width: 960px) {
       text-align: center !important;
     }
   }
